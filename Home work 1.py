60686b9e11cb95402735e389b4aea966c5a8f197
nuamah toffick <tofficknuamahrafat.com>
2025-05-03 02:05:24 -0800

    first python commit

---
+++ Home work 1.py
@@ -0,0 +1,12 @@
+#The area of a rectangle
+length = float(input("Enter the length of the rectanglle:")
+width = float(input("Enter the width of the rectangle:")
+print("The area of the rectangle is :")              
+
+
+
+#experimenting with different variables
+product_price = float(input("enter the price of product?"))
+product_number = float(input("enter the number of product?"))
+total = product_price * product_number
+print(total)  
---
+++ practice 1.py
@@ -0,0 +1,8 @@
+#a program that asks for the user's name and age
+name = input("What is your name?");
+age = input("What is your age?")
+print("Hello my name is",name,"and i am",age,"years old.")
+
+
+                     
+                    
