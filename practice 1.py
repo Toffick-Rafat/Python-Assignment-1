662a8c6b382f2446f798050dad2847eeba85197d
nuamah toffick <tofficknuamahrafat.com>
2025-05-03 19:06:37 -0800

    correction

--- practice 1.py
+++ practice 1.py
@@ -1,5 +1,5 @@
 #a program that asks for the user's name and age
-name = input("What is your name?");
+name = input("What is your name?")
 age = input("What is your age?")
 print("Hello my name is",name,"and i am",age,"years old.")
 
